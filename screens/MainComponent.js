3f178d4dde2e7fccd10d2afe9eef5060dcde90a1
Darshil Patel <darshilpatel1990@gmail.com>
2023-03-22 12:37:11 -0500

    Navigation Part 1

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,22 +1,45 @@
-import { useState } from 'react';
-import { View } from 'react-native';
-import { CAMPSITES } from '../shared/campsites';
+import { Platform, View } from 'react-native';
+import Constants from 'expo-constants';
+import { createStackNavigator } from '@react-navigation/stack';
 import DirectoryScreen from './DirectoryScreen';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 
-const Main = () => {
-    const [campsites, setCampsites] = useState(CAMPSITES);
-    const [selectedCampsiteId, setSelectedCampsiteId] = useState();
-
+const DirectoryNavigator = () => {
+    const Stack = createStackNavigator();
     return (
-        <View style={{ flex: 1 }}>
-            <DirectoryScreen
-                campsites={campsites}
-                onPress={campsiteId => setSelectedCampsiteId(campsiteId)}
+        <Stack.Navigator
+            initialRouteName='Directory'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#5637DD'
+                },
+                headerTintColor: '#fff'
+            }}
+        >
+            <Stack.Screen
+                name='Directory'
+                component={DirectoryScreen}
+                options={{ title: 'Campsite Directory' }}
             />
-            <CampsiteInfoScreen
-                campsite={campsites.filter(campsite => campsite.id === selectedCampsiteId)[0]}
+            <Stack.Screen
+                name='CampsiteInfo'
+                component={CampsiteInfoScreen}
+                options={({ route }) => ({
+                    title: route.params.campsite.name
+                })}
             />
+        </Stack.Navigator>
+    );
+};
+
+const Main = () => {
+    return (
+        <View style={{
+            flex: 1,
+            paddingTop:
+                Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+        }}>
+            <DirectoryNavigator />
         </View>
     );
 };
