2d2f858eb0bb1966d154d977404b88f97c2495bb
Darshil Patel <darshilpatel1990@gmail.com>
2023-04-04 21:30:27 -0500

    Gestures Part 1 & 2

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,10 +1,47 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, PanResponder, Alert } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable'
+import { useRef } from "react";
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
+
+    const view = useRef();
+
+    const isLeftSwipe = ({ dx }) => dx < -200;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current
+                .rubberBand(1000)
+                .then((endState) => console.log(endState.finished ? 'finished' : 'canceled'))
+        },
+        onPanResponderEnd: (e, gestureState) => {
+            console.log(gestureState);
+            if (isLeftSwipe(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () =>
+                                props.isFavorite
+                                    ? console.log('Already set as a favorite')
+                                    : props.markFavorite()
+                        }
+                    ]
+                )
+            }
+        }
+    });
 
     if (campsite) {
         return (
@@ -12,6 +49,8 @@ const RenderCampsite = (props) => {
                 animation='fadeInDownBig'
                 duration={2000}
                 delay={1000}
+                ref={view}
+                {...panResponder.panHandlers}
             >
                 <Card containerStyle={styles.cardContainer}>
                     <Card.Image source={{ uri: baseUrl + campsite.image }}>
